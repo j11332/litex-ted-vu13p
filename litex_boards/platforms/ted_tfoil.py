8aacb800cb6513807ea45c5c4e579383aff94b10
harasawa <harasawa@rz9.int.lila.cs.tsukuba.ac.jp>
2021-04-01 20:11:57 +0900

    first commit

---
+++ litex_boards/platforms/ted_tfoil.py
@@ -0,0 +1,167 @@
+# This file is part of LiteX-Boards.
+# SPDX-License-Identifier: BSD-2-Clause
+from litex.build.generic_platform import *
+from litex.build.xilinx import XilinxPlatform, VivadoProgrammer
+
+# IOs
+_io = [
+    # Clk / Rst
+    ("sys_clk_0", 0,
+        Subsignal("p", Pins("BE22"), IOStandard("DIFF_SSTL12")),
+        Subsignal("n", Pins("BF22"), IOStandard("DIFF_SSTL12")),
+    ),
+    ("sys_clk_1", 0,
+        Subsignal("p", Pins("N27"), IOStandard("DIFF_SSTL12")),
+        Subsignal("n", Pins("M27"), IOStandard("DIFF_SSTL12")),
+    ),
+    ("clk125", 0, Pins("BD16"), IOStandard("LVCMOS18")),
+    
+    # PSW[0] - Active-low
+    ("cpu_resetn", 0, Pins("P36"), IOStandard("LVCMOS12")),
+
+    # Leds
+    ("user_led", 7, Pins("AW15"), IOStandard("LVCMOS18")),
+    ("user_led", 6, Pins("AV16"), IOStandard("LVCMOS18")),
+    ("user_led", 5, Pins("BA15"), IOStandard("LVCMOS18")),
+    ("user_led", 4, Pins("AY15"), IOStandard("LVCMOS18")),
+    ("user_led", 3, Pins("AV17"), IOStandard("LVCMOS18")),
+    ("user_led", 2, Pins("AU17"), IOStandard("LVCMOS18")),
+    ("user_led", 1, Pins("AY16"), IOStandard("LVCMOS18")),
+    ("user_led", 0, Pins("AW16"), IOStandard("LVCMOS18")),
+
+    # Serial
+    ("serial", 0,
+        Subsignal("rx",  Pins("C30"), IOStandard("LVCMOS18")),
+        Subsignal("tx",  Pins("C29"), IOStandard("LVCMOS18")),
+    ),
+
+    # I2C
+    ("i2c_tca9548", 0, 
+        Subsignal("SDA", Pins("BD21"), IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("BA20"), IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9548", 1, 
+        Subsignal("SDA", Pins("BB19"), IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("BB20"), IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9548", 2, 
+        Subsignal("SDA", Pins("AW21"), IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("BD19"), IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9548", 3, 
+        Subsignal("SDA", Pins("AY22"), IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("BC21"), IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 0, 
+        Subsignal("SDA", Pins("A30"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("A28"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 1, 
+        Subsignal("SDA", Pins("C28"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("B29"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 2, 
+        Subsignal("SDA", Pins("D29"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("E30"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 3, 
+        Subsignal("SDA", Pins("F28"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("F29"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 4, 
+        Subsignal("SDA", Pins("G29"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("G30"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 5, 
+        Subsignal("SDA", Pins("K30"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("J30"),  IOStandard("LVCMOS18"))
+    ),
+    ("i2c_tca9555", 6, 
+        Subsignal("SDA", Pins("J31"),  IOStandard("LVCMOS18")),
+        Subsignal("SCL", Pins("J29"),  IOStandard("LVCMOS18"))
+    ),
+
+    # DDR4 memory channel C0. Only use the first 64 data bits
+    ("ddram", 0,
+        Subsignal("a", Pins(
+            "BE25 AW23 BC23 AY23 BC24 BD23 BB24 BE23",
+            "BE20 BC22 AY25 BF25 BE21 BA27"),
+            IOStandard("SSTL12_DCI")),
+        Subsignal("ba",        Pins("BB25 BA25"), IOStandard("SSTL12_DCI")),
+        Subsignal("bg",        Pins("BA23 BD24"), IOStandard("SSTL12_DCI")),
+        Subsignal("ras_n",     Pins("BD26"), IOStandard("SSTL12_DCI")), # A16
+        Subsignal("cas_n",     Pins("BB26"), IOStandard("SSTL12_DCI")), # A15
+        Subsignal("we_n",      Pins("AW24"), IOStandard("SSTL12_DCI")), # A14
+        Subsignal("cs_n",      Pins("BC26"), IOStandard("SSTL12_DCI")),
+        Subsignal("act_n",     Pins("BA24"), IOStandard("SSTL12_DCI")),
+        Subsignal("dm",        Pins(
+            "BL24 BH28 BG26 BC28 BA28 BE31 BD35 AW35"), IOStandard("POD12_DCI")),
+        Subsignal("dq",        Pins(
+            "BK25 BL25 BH24 BH23 BK22 BL22 BJ25 BJ24",
+            "BK30 BL30 BJ26 BK26 BL27 BL28 BJ29 BJ30",
+            "BF27 BF28 BE26 BE27 BF29 BG30 BG29 BH29",
+            "BB31 BC31 BB29 BC29 BD28 BE28 BD30 BE30",
+            "AV29 AW29 AW31 AY31 AW28 AY28 AY30 BA30",
+            "BC33 BD33 BC34 BD34 BB32 BC32 AY32 BA32",
+            "BA35 BB35 BA34 BB34 BC37 BD37 BA37 BB37",
+            "AW33 AW34 AU34 AV34 AY35 AY36 AV36 AV37"),
+            IOStandard("POD12_DCI"),
+            Misc("PRE_EMPHASIS=RDRV_240"),
+            Misc("EQUALIZATION=EQ_LEVEL2")),
+        Subsignal("dqs_p",      Pins(
+            "BJ23 BK27 BH26 BB27 AU31 AY33 BB36 AV32"),
+		    IOStandard("DIFF_POD12"),
+            Misc("PRE_EMPHASIS=RDRV_240"),
+            Misc("EQUALIZATION=EQ_LEVEL2")),
+        Subsignal("dqs_n",   Pins(
+            "BK23 BK28 BH27 BC27 AV31 BA33 BC36 AV33"),
+		    IOStandard("DIFF_POD12"),
+            Misc("PRE_EMPHASIS=RDRV_240"),
+            Misc("EQUALIZATION=EQ_LEVEL2")),
+        Subsignal("clk_p",   Pins("BG25"), IOStandard("DIFF_SSTL12_DCI")),
+        Subsignal("clk_n",   Pins("BG24"), IOStandard("DIFF_SSTL12_DCI")),
+        Subsignal("cke",     Pins("BB22"), IOStandard("SSTL12_DCI")),
+        Subsignal("odt",     Pins("AW25"), IOStandard("SSTL12_DCI")),
+        Subsignal("reset_n", Pins("AV26"), IOStandard("LVCMOS12")),
+        Misc("SLEW=FAST"),
+    ),
+]
+
+# Connectors
+_connectors = []
+
+# Platform
+class Platform(XilinxPlatform):
+    default_clk_name   = "sys_clk_0"
+    default_clk_period = 1e9/200e6
+
+    def __init__(self):
+        XilinxPlatform.__init__(self, "xcvu13p-flga2577-2-e", _io, _connectors, toolchain="vivado")
+
+    def create_programmer(self):
+        return VivadoProgrammer()
+
+    def do_finalize(self, fragment):
+        XilinxPlatform.do_finalize(self, fragment)
+        self.add_period_constraint(self.lookup_request("sys_clk_0", loose=True), 1e9/200e6)
+        self.add_period_constraint(self.lookup_request("sys_clk_1", loose=True), 1e9/200e6)
+        self.add_period_constraint(self.lookup_request("clk125",    loose=True), 1e9/125e6)
+
+        self.add_platform_command("set_property CONFIG_VOLTAGE 1.8                      [current_design]")
+        self.add_platform_command("set_property CFGBVS GND                              [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.EXTMASTERCCLK_EN DIV-1 [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.CONFIGRATE 31.9        [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.SPI_32BIT_ADDR YES     [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 8         [current_design]")
+        self.add_platform_command("set_property BITSTREAM.GENERAL.COMPRESS TRUE         [current_design]")
+        self.add_platform_command("set_property BITSTREAM.CONFIG.SPI_FALL_EDGE YES      [current_design]")
+
+        # DDR4 memory channel C1 Internal Vref
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 61]")
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 62]")
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 63]")
+
+        # DDR4 memory channel C2 Internal Vref
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 73]")
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 74]")
+        self.add_platform_command("set_property INTERNAL_VREF 0.84 [get_iobanks 75]")
---
+++ ted_tfoil.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+#
+# This file is part of LiteX-Boards.
+#
+# Copyright (c) 2020 Fei Gao <feig@princeton.edu>
+# Copyright (c) 2020 Florent Kermarrec <florent@enjoy-digital.fr>
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os
+import argparse
+
+from migen import *
+from migen.genlib.resetsync import AsyncResetSynchronizer
+
+from litex_boards.platforms import ted_tfoil
+
+from litex.soc.cores.clock import *
+from litex.soc.integration.soc_core import *
+from litex.soc.integration.builder import *
+from litex.soc.cores.led import LedChaser
+
+from litedram.modules import MT40A1G8
+from litedram.phy import usddrphy
+
+# CRG
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.rst = Signal()
+        self.clock_domains.cd_sys    = ClockDomain()
+        self.clock_domains.cd_sys4x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_pll4x  = ClockDomain(reset_less=True)
+        self.clock_domains.cd_idelay = ClockDomain()
+
+        self.submodules.pll = pll = USMMCM(speedgrade=-2)
+        self.comb += pll.reset.eq((~platform.request("cpu_resetn")) | self.rst)
+        pll.register_clkin(platform.request("sys_clk_0"), 200e6)
+        pll.create_clkout(self.cd_pll4x, sys_clk_freq*4, buf=None, with_reset=False)
+        pll.create_clkout(self.cd_idelay, 400e6)
+        platform.add_false_path_constraints(self.cd_sys.clk, pll.clkin) # Ignore sys_clk to pll.clkin path created by SoC's rst.
+
+        self.specials += [
+            Instance("BUFGCE_DIV", name="main_bufgce_div",
+                p_BUFGCE_DIVIDE=4,
+                i_CE=1, i_I=self.cd_pll4x.clk, o_O=self.cd_sys.clk),
+            Instance("BUFGCE", name="main_bufgce",
+                i_CE=1, i_I=self.cd_pll4x.clk, o_O=self.cd_sys4x.clk),
+        ]
+
+        self.submodules.idelayctrl = USIDELAYCTRL(cd_ref=self.cd_idelay, cd_sys=self.cd_sys)
+
+# BaseSoC
+
+class BaseSoC(SoCCore):
+    def __init__(self, sys_clk_freq=int(200e6), **kwargs):
+        platform = ted_tfoil.Platform()
+
+        # SoCCore 
+        SoCCore.__init__(self, platform, sys_clk_freq,
+            ident          = "LiteX SoC on tfoil",
+            ident_version  = True,
+            **kwargs)
+
+        # CRG
+        self.submodules.crg = _CRG(platform, sys_clk_freq)
+
+        # DDR4 SDRAM
+        if not self.integrated_main_ram_size:
+            self.submodules.ddrphy = usddrphy.USPDDRPHY(platform.request("ddram"),
+                memtype          = "DDR4",
+                sys_clk_freq     = sys_clk_freq,
+                iodelay_clk_freq = 400e6)
+            self.add_sdram("sdram",
+                phy           = self.ddrphy,
+                module        = MT40A1G8(sys_clk_freq, "1:4"),
+                size          = 0x40000000,
+                l2_cache_size = kwargs.get("l2_size", 8192)
+            )
+
+        # Leds
+        self.submodules.leds = LedChaser(
+            pads         = platform.request_all("user_led"),
+            sys_clk_freq = sys_clk_freq)
+        
+
+# Build
+
+def main():
+    parser = argparse.ArgumentParser(description="LiteX SoC on tfoil")
+    parser.add_argument("--build",        action="store_true", help="Build bitstream")
+    parser.add_argument("--load",         action="store_true", help="Load bitstream")
+    parser.add_argument("--sys-clk-freq", default=200e6,       help="System clock frequency (default: 200MHz)")
+    builder_args(parser)
+    soc_core_args(parser)
+    args = parser.parse_args()
+
+    soc = BaseSoC(
+        sys_clk_freq = int(float(args.sys_clk_freq)),
+        **soc_core_argdict(args)
+    )
+    builder = Builder(soc, **builder_argdict(args))
+    builder.build(run=args.build)
+
+    if args.load:
+        prog = soc.platform.create_programmer()
+        prog.load_bitstream(os.path.join(builder.gateware_dir, soc.build_name + ".bit"))
+
+if __name__ == "__main__":
+    main()
